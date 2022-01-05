7eeba0acffc1ea311baa8950af52291c1b224c80
lhdai <d_muses@163.com>
2022-01-05 10:03:58 +0800

    s

---
+++ src/lib/vue-amazing-select/index.js
@@ -0,0 +1,7 @@
+import VueAmazingSelecter from './lib/VueAmazingSelecter.vue'
+
+VueAmazingSelecter.install = (Vue) => {
+  Vue.component(VueAmazingSelecter.name, VueAmazingSelecter)
+}
+
+export default VueAmazingSelecter
