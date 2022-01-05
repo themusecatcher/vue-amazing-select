d24dd0e9760d039b388ced311b56ff164ab4b2ac
lhdai <d_muses@163.com>
2022-01-05 16:48:55 +0800

    s

--- vue.config.js
+++ vue.config.js
@@ -1,23 +1,11 @@
-// vue-config.js
 module.exports = {
-  // 修改 src 目录 为 examples 目录
-  pages: {
-    index: {
-      // page 的入口
-      entry: 'src/main.js',   // 把src 修改为examples
-      // 模板来源
-      template: 'public/index.html',
-      // 在 dist/index.html 的输出
-      filename: 'index.html'
-    }
-  },
   // 扩展 webpack 配置，使 packages 加入编译
-  /* chainWebpack 是一个函数，会接收一个基于 webpack-chain 的 ChainableConfig 实例。允许对内部的 webpack 配置进行更细粒度的修改。 */
+  // chainWebpack 是一个函数，会接收一个基于 webpack-chain 的 ChainableConfig 实例。允许对内部的 webpack 配置进行更细粒度的修改
   chainWebpack: config => {
     config.module
       .rule('js')
       .include
-      .add(__dirname + 'packages')  // 注意这里需要绝对路径，所有要拼接__dirname
+      .add(__dirname + 'packages') // 注意这里需要绝对路径，所以要拼接__dirname
       .end()
       .use('babel')
       .loader('babel-loader')
